fac1bd9b15d68e13e2a2f5b10ba376be2b458038
Tyson Key <tyson.key@gmail.com>
2016-10-17 09:50:38 +0100

    Network status observer

--- app/src/main/java/example/com/astateofstyle/MainActivity.java
+++ app/src/main/java/example/com/astateofstyle/MainActivity.java
@@ -3,12 +3,19 @@ package example.com.astateofstyle;
 import android.content.res.AssetFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.github.pwittchen.reactivenetwork.library.Connectivity;
+import com.github.pwittchen.reactivenetwork.library.ReactiveNetwork;
 
 import java.util.List;
 
 import example.com.astateofstyle.models.categories.Category;
+import rx.Observable;
 import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 import rx.subscriptions.CompositeSubscription;
 
@@ -24,6 +31,21 @@ public class MainActivity extends AppCompatActivity {
 
         iCoreApiController = AsosSvc.PrepareAsosApi();
 
+        //Watch for network connectivity changes
+
+        ReactiveNetwork.observeInternetConnectivity()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Boolean>() {
+                    @Override public void call(Boolean isConnectedToInternet) {
+                       Toast netStatus = new Toast(getApplicationContext());
+                        netStatus.setText("No Network Connectivity");
+                        netStatus.setDuration(Toast.LENGTH_LONG);
+                        netStatus.show();
+                    }
+                });
+
+
         //Initialise the RxJava/Retrofit API handlers
        // iCoreApiController
 
@@ -32,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<List<Category>>() {
                     @Override
                     public void onCompleted() {
-
+                        Log.d("AsosSubs", "Finished fetching data...");
                     }
 
                     @Override
@@ -42,7 +64,10 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onNext(List<Category> categories) {
+                        Log.d("AsosSubs", "Got categories: " + categories.size() + " Also : " +Observable.from(categories).count());
+                        Log.d("AsosSubs", categories.toString());
 
+                        ;
                     }
                 }));
 
